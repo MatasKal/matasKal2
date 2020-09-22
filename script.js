e0e0a0f477c9a1000a5b95f5932aa8071e577772
MatasKal <49563425+MatasKal@users.noreply.github.com>
2020-09-22 20:29:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
+const url = 'http://78.63.13.74:3006/FlowFormaAPI/names';
 const ul = document.getElementById('authors');
-const url = 'https://randomuser.me/api/?results=10';
+
 fetch(url)
-.then((resp) => resp.json())
+.then((resp) => resp.json()) // Transform the data into json
 .then(function(data) {
-  let authors = data.results;
-  return authors.map(function(author) {
-    let li = createNode('li'),
-        img = createNode('img'),
-        span = createNode('span');
-    img.src = author.picture.medium;
-    span.innerHTML = `${author.name.first} ${author.name.last}`;
-    append(li, img);
-    append(li, span);
-    append(ul, li);
+    let authors = data.results; // Get the results
+    return authors.map(function(author) { // Map through the results and for each run the code below
+      let li = createNode('li'); //  Create the elements we nee
+      // Add the source of the image to be the src of the img element
+      span.innerHTML = ${author}; // Make the HTML of our span to be the first and last name of our author
+      append(ul, li);
+    })
   })
-})
-.catch(function(error) {
-  console.log(error);
-});
